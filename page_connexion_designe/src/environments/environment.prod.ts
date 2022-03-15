dda56e85b18902ee0ca5417d3f8c1bbc8df7abf8
GAGOER <fabian.afiankoe@gmail.com>
2022-03-15 16:31:03 -0300

    La connexion entre firebase et mon application a ete fait, et cela grace a Saint-Aimé. Il y a une carte mentale qui a ete fait pour que je puisse la reproduire. Maintenant l'objectif est de faire l'API de connexion et de faire une page de profile dans lesquelle oon pourra se connecter

--- page_connexion_designe/src/environments/environment.prod.ts
+++ page_connexion_designe/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'connexion-ac941',
+    appId: '1:135729457222:web:8b3c8a3919e9b14fee4247',
+    storageBucket: 'connexion-ac941.appspot.com',
+    apiKey: 'AIzaSyDUoC2Cjz3mWepdItWPEBSW0AOlM0t_Wio',
+    authDomain: 'connexion-ac941.firebaseapp.com',
+    messagingSenderId: '135729457222',
+    measurementId: 'G-XF8HKSB4QJ',
+  },
   production: true
 };
